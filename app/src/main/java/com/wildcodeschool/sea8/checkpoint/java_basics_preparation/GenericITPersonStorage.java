b95e06d14872caf295d678a5955a3ae56f21aa9c
Andreas Schlagner <andreas-schlagner_student2022@wilder.school>
2022-04-05 14:21:37 +0200

    add GenericITPersonStorage as a first attempt

---
+++ app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/GenericITPersonStorage.java
@@ -0,0 +1,29 @@
+package com.wildcodeschool.sea8.checkpoint.java_basics_preparation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericITPersonStorage<T extends ITPerson> {
+
+    // Attributes
+    private List<T> persons;
+
+    // Constructor
+    public GenericITPersonStorage() {
+        this.persons = new ArrayList<>();
+    }
+
+    // "Setter & Getter" for List entries
+    public boolean storePerson(T object) {
+        return this.persons.add(object);
+    }
+
+    public T getLastStoredPerson() {
+        return this.persons.get(persons.size()-1);
+    }
+
+    public T getFirstStoredPerson() {
+        return this.persons.get(0);
+    }
+
+}
