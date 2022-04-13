1e5a78062812d82ed7aa90744c0b08305d39e0c3
Andreas Schlagner <andreas-schlagner_student2022@wilder.school>
2022-04-13 16:09:57 +0200

    warning weg

--- app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/ITPersonStorage.java
+++ app/src/main/java/com/wildcodeschool/sea8/checkpoint/java_basics_preparation/ITPersonStorage.java
@@ -8,7 +8,7 @@ import java.util.List;
  */
 public class ITPersonStorage {
 
-    List persons = new ArrayList<>();
+    List<Object> persons = new ArrayList<>();
 
     public boolean storePerson(Object person) {
         return persons.add(person);
